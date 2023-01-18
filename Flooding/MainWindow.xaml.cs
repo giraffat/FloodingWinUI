ad8bada3a17dc28c8c67768b3759c4ec7a5848ec
giraf <giraffat@outlook.com>
2023-01-18 22:18:16 +0800

    配置项目，配置窗口，增加教程按钮，优化布局

--- Flooding/MainWindow.xaml.cs
+++ Flooding/MainWindow.xaml.cs
@@ -1,9 +1,18 @@
+using WinUIEx;
+
 namespace Flooding
 {
     internal sealed partial class MainWindow
     {
         public MainWindow()
         {
+            this.Title = "к╒фа";
+            this.SetWindowSize(300,500);
+            this.SetIsAlwaysOnTop(true);
+            this.SetIsResizable(false);
+            this.SetIsMaximizable(false);
+            this.SetIsMinimizable(false);
+
             InitializeComponent();
         }
     }
