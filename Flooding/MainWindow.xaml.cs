773a9c4900eb2f40e293af9caee3a50414f63287
giraf <giraffat@outlook.com>
2023-01-20 14:52:11 +0800

    增加无限模式选择框

--- Flooding/MainWindow.xaml.cs
+++ Flooding/MainWindow.xaml.cs
@@ -1,13 +1,21 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using Drastic.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System.ComponentModel;
+using System.Diagnostics;
 using WinUIEx;
 
 namespace Flooding
 {
-    internal sealed partial class MainWindow
+    internal sealed partial class MainWindow: INotifyPropertyChanged
     {
+        private readonly MainWindowViewmodel _viewmodel = new();
+
         public MainWindow()
         {
-            this.Title = "к╒фа";
-            this.SetWindowSize(300,500);
+            Title = "к╒фа";
+
+            this.SetWindowSize(300, 500);
             this.SetIsAlwaysOnTop(true);
             this.SetIsResizable(false);
             this.SetIsMaximizable(false);
@@ -15,5 +23,11 @@ namespace Flooding
 
             InitializeComponent();
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool BoolNegation(bool value) => !value;
     }
+    
+    
 }
---
+++ Flooding/MainWindowModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Flooding;
+
+public class MainWindowModel
+{
+    
+}
